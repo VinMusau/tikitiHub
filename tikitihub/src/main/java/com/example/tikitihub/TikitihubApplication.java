a84f9811c3d2688b098d282a8a300db214d6b1fb
VinMusau <musauvinnie@outlook.com>
2026-09-23 12:52:06 +0300

    fix: add dotenv-java dependency and configure environment variables loading

--- tikitihub/src/main/java/com/example/tikitihub/TikitihubApplication.java
+++ tikitihub/src/main/java/com/example/tikitihub/TikitihubApplication.java
@@ -4,11 +4,17 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.scheduling.annotation.EnableAsync;
 
+import io.github.cdimascio.dotenv.Dotenv;
+
 @SpringBootApplication
 @EnableAsync
 public class TikitihubApplication {
 
 	public static void main(String[] args) {
+		Dotenv dotenv = Dotenv.configure().ignoreIfMissing().load();
+		dotenv.entries().forEach(entry -> {
+			System.setProperty(entry.getKey(), entry.getValue());
+		});
 		SpringApplication.run(TikitihubApplication.class, args);
 	}
 
